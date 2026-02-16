bae6f24d2c58fd4aa03de8183abb96681bcccb0d
SviatoslavHurskyi <svyat454@gmail.com>
2026-02-16 17:41:54 +0100

    Add project React

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// Lessons
+import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
+import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
+import Lesson_05 from "./lessons/Lesson_05/Lesson_05";
+
+// Homeworks
+import Homework_02 from "./homeworks/Homework_02/Homework_02";
+import Homework_03 from "./homeworks/Homework_03/Homework_03";
+import Homework_04 from "./homeworks/Homework_04/Homework_04";
+
+function App() {
+  return (
+    <>
+      {/* LESSONS */}
+      {/* <Lesson_02 /> */}
+      {/* <Lesson_03 /> */}
+      {/* <Lesson_04 /> */}
+      <Lesson_05 />
+      {/* HOMEWORKS */}
+      {/* <Homework_02 /> */}
+      {/* <Homework_03 /> */}
+      {/* <Homework_04 /> */}
+    </>
+  );
+}
+
+export default App;
